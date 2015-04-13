b9029057181f9bc24fa7dd3321b40f2bbbda2a42
TaylorM1 <appliedgenius@gmail.com>
2015-04-12 19:33:48 -0700

    PHPMailer + Gmail SMTP

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,30 @@
 <?php
+require_once('phpmailer/class.phpmailer.php');
+define('GUSER', 'taylor.miles42@gmail.com'); // GMail username
+define('GPWD', 'blockland12'); // GMail password
+function smtpmailer($to, $from, $from_name, $subject, $body) { 
+	global $error;
+	$mail = new PHPMailer();  // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true;  // authentication enabled
+	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465; 
+	$mail->Username = GUSER;  
+	$mail->Password = GPWD;           
+	$mail->SetFrom($from, $from_name);
+	$mail->Subject = $subject;
+	$mail->Body = $body;
+	$mail->AddAddress($to);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Message sent!';
+		return true;
+	}
+}
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -19,8 +45,9 @@ $message = $_POST['message'];
 $to = 'taylor.miles42@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@taylormiles.herokuapp.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@herokuapp.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
+smtpmailer($to, $email_address, $name, $email_subject, $email_body);
 return true;			
 ?>
